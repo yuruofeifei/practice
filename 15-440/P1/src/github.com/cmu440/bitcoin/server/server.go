8e9472041cb70dc2813feaec6e623cb061f6933a
Ruofei Yu <yuruofei@amazon.com>
2019-01-16 13:51:01 -0800

    P2 finish

--- 15-440/P1/src/github.com/cmu440/bitcoin/server/server.go
+++ 15-440/P1/src/github.com/cmu440/bitcoin/server/server.go
@@ -1,70 +1,226 @@
-// The scheduling strategy of the server is as follows:
-// 1. The server maintains a queue of jobs to be done
-// 2. When a request comes from a client, split the request into multiple sub-jobs,
-//    each job has a maximum job size limit. Append all sub-jobs to the job queue
-// 3. Whenever a miner becomes idle or new miner is joined, take a new job from the
-//    job queue
-// 4. Whenever a miner is lost, its job is prepended back to the job queue, waiting
-//    for the next available miner to take over the job
 package main
 
 import (
-	"container/list"
-	"encoding/json"
 	"fmt"
-	"github.com/cmu440/bitcoin"
-	"github.com/cmu440/lsp"
+	"log"
 	"os"
 	"strconv"
+	"encoding/json"
+	"container/list"
+	"github.com/cmu440/bitcoin"
+	"github.com/cmu440/lsp"
 )
 
-const maxJobSize = 4000
-
-type idMsgBundle struct {
-	id  int
-	msg *bitcoin.Message
-}
-
-type job struct {
-	clientid int
-	message  string
-	minNonce uint64
-	maxNonce uint64
-}
-
-// Storing the progress and result of each client request
-type request struct {
-	jobsRemain int
-	minHash    uint64
-	nonce      uint64
-}
+const (
+	unitWorkload=1000000
+)
 
 type server struct {
-	lspServer  lsp.Server
-	jobQueue   *list.List
-	miners     map[int]*job
-	clients    map[int]*request
-	receiveMsg chan idMsgBundle
-	connLost   chan int
+	lspServer lsp.Server
+	serverClosingChan chan bool
+	rcvMsgQChan chan *list.List
+	minersMap map[int]*list.Element
+	clientsResultsMap map[int]*Result
+	freeL *list.List
+	busyL *list.List
+	jobL *list.List
+	jobsMap map[int]*Job
+	jobid int
+}
+
+type Result struct {
+	empty bool
+	hash uint64
+	nonce uint64
+	nUnfinished int
+}
+
+type miner struct {
+	mid int
+	working bool
+	jobid int
+}
+
+type Job struct {
+	cid int
+	jobid int
+	data string
+	lower uint64
+	upper uint64
+}
+
+type Msg struct {
+	connid int
+	payload []byte
+	err error
 }
 
 func startServer(port int) (*server, error) {
-	lspserver, err := lsp.NewServer(port, lsp.NewParams())
+	// TODO: implement this!
+	lspSrv, err := lsp.NewServer(port, lsp.NewParams())
 	if err != nil {
 		return nil, err
 	}
 	srv := &server{
-		lspServer:  lspserver,
-		jobQueue:   list.New(),
-		miners:     make(map[int]*job),
-		clients:    make(map[int]*request),
-		receiveMsg: make(chan idMsgBundle),
-		connLost:   make(chan int),
+		lspServer: lspSrv, 
+		serverClosingChan: make(chan bool),
+		rcvMsgQChan: make(chan *list.List, 1),
+		minersMap: make(map[int]*list.Element),
+		clientsResultsMap: make(map[int]*Result),
+		freeL: list.New(),
+		busyL: list.New(),
+		jobL: list.New(),
+		jobsMap: make(map[int]*Job),
 	}
+	srv.rcvMsgQChan <- list.New()
 	return srv, nil
 }
 
+func (s *server) readRoutine() {
+	for {
+		select {
+			case <- s.serverClosingChan:
+				return
+			default:
+				connid, payload, err := s.lspServer.Read()
+				q := <- s.rcvMsgQChan
+				q.PushBack(&Msg{connid: connid, payload: payload, err:err})
+				s.rcvMsgQChan <- q
+		}
+	}
+}
+
+// similar to cpu round robin slicing
+func (s *server) mainRoutine() {
+	for {
+		select {
+			case <- s.serverClosingChan:
+				return
+			case rcvQ := <- s.rcvMsgQChan:
+				if rcvQ.Len() > 0 {
+ 					head := rcvQ.Front()
+ 					rcvQ.Remove(head)
+ 					m := head.Value.(*Msg)
+					connid := m.connid
+					payload := m.payload
+					err := m.err
+					if err != nil { // miner or client connection lost
+						if minerE, ok := s.minersMap[connid]; ok {
+							miner := minerE.Value.(*miner)
+							jobid := miner.jobid
+							if miner.working {
+								s.busyL.Remove(minerE)
+								s.jobL.PushBack(s.jobsMap[jobid])
+							} else {
+								s.freeL.Remove(minerE)
+							}
+							delete(s.minersMap, connid)
+						} else {
+							delete(s.clientsResultsMap, connid)
+						}
+						s.lspServer.CloseConn(connid)
+					} else {
+						var msg bitcoin.Message
+						json.Unmarshal(payload, &msg)
+						switch msg.Type {
+							case bitcoin.Join:
+								s.freeL.PushBack(&miner{working: false, mid: connid})
+								s.minersMap[connid] = s.freeL.Back()
+							case bitcoin.Request:
+								data := msg.Data
+								lower := msg.Lower
+								upper := msg.Upper
+								s.clientsResultsMap[connid] = &Result{hash:0, nonce:0, empty:true}
+								count := 0
+								for i := lower; i < upper; i += unitWorkload {
+									u := i + unitWorkload
+									if upper < u {
+										u = upper
+									}
+									job := &Job{jobid: s.jobid, data: data, lower: i, upper: u, cid: connid}
+									s.jobL.PushBack(job)
+									s.jobsMap[s.jobid] = job
+									s.jobid++
+									count++
+								}
+								s.clientsResultsMap[connid].nUnfinished = count
+							case bitcoin.Result:
+								hash := msg.Hash
+								nonce := msg.Nonce
+								minerE := s.minersMap[connid]
+								miner := minerE.Value.(*miner)
+								jobid := miner.jobid 
+								job := s.jobsMap[jobid]
+								cid := job.cid
+								s.busyL.Remove(minerE)
+								miner.working = false
+								miner.jobid = -1
+								s.freeL.PushBack(miner)
+								delete(s.jobsMap, jobid)
+								if partialResult, ok := s.clientsResultsMap[cid]; ok {
+									if partialResult.empty || hash < partialResult.hash {
+										partialResult.nonce = nonce
+										partialResult.hash = hash
+										partialResult.empty = false
+									}
+									partialResult.nUnfinished--
+									if partialResult.nUnfinished == 0 {
+										resultMsg := bitcoin.NewResult(partialResult.hash, partialResult.nonce)
+										payload, _ := json.Marshal(resultMsg)
+										s.lspServer.Write(cid, payload)
+										delete(s.clientsResultsMap, cid)
+									}
+								}
+						}
+					}
+				}
+				s.rcvMsgQChan <- rcvQ
+		}
+		for s.jobL.Len() > 0 {
+			head := s.jobL.Front()
+			job := head.Value.(*Job)
+			if _, ok := s.clientsResultsMap[job.cid]; ok {
+				if s.freeL.Len() > 0 {
+					minerE := s.freeL.Front()
+					s.freeL.Remove(minerE)
+					miner := minerE.Value.(*miner)
+					s.minersMap[miner.mid] = minerE
+					miner.working = true 
+					miner.jobid = job.jobid
+					s.busyL.PushBack(miner)
+					requestMsg := bitcoin.NewRequest(job.data, job.lower, job.upper)
+					payload, _ := json.Marshal(requestMsg)
+					s.lspServer.Write(miner.mid, payload)
+					s.jobL.Remove(head)
+				} else {
+					break
+				}
+			} else {
+				s.jobL.Remove(head)
+			}
+		}
+	}
+}
+
+var LOGF *log.Logger
+
 func main() {
+	// You may need a logger for debug purpose
+	const (
+		name = "log.txt"
+		flag = os.O_RDWR | os.O_CREATE
+		perm = os.FileMode(0666)
+	)
+
+	file, err := os.OpenFile(name, flag, perm)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	LOGF = log.New(file, "", log.Lshortfile|log.Lmicroseconds)
+	// Usage: LOGF.Println() or LOGF.Printf()
+
 	const numArgs = 2
 	if len(os.Args) != numArgs {
 		fmt.Printf("Usage: ./%s <port>", os.Args[0])
@@ -79,122 +235,16 @@ func main() {
 
 	srv, err := startServer(port)
 	if err != nil {
-		fmt.Println("Failed to start server:", err)
+		fmt.Println(err.Error())
 		return
 	}
 	fmt.Println("Server listening on port", port)
 
 	defer srv.lspServer.Close()
+	defer close(srv.serverClosingChan)
 
-	go srv.receiveMessage()
-
-	srv.handleMessage()
-}
-
-func (s *server) handleMessage() {
-	for {
-		select {
-		case bundle := <-s.receiveMsg:
-			id := bundle.id
-			msg := bundle.msg
-			switch msg.Type {
-			case bitcoin.Join:
-				s.miners[id] = nil // Add a miner to the miner pool
-			case bitcoin.Request:
-				s.addJob(id, msg)
-			case bitcoin.Result:
-				clientId := s.miners[id].clientid
-				s.miners[id] = nil
-
-				if clientJob, exist := s.clients[clientId]; exist {
-					clientJob.jobsRemain--
-					if msg.Hash < s.clients[clientId].minHash {
-						clientJob.minHash = msg.Hash
-						clientJob.nonce = msg.Nonce
-					}
-					// Client's job finished. Return result to client
-					if clientJob.jobsRemain == 0 {
-						s.sendMessage(clientId, bitcoin.NewResult(clientJob.minHash, clientJob.nonce))
-						s.lspServer.CloseConn(clientId)
-						delete(s.clients, clientId)
-					}
-				}
-			}
-		case id := <-s.connLost:
-			if _, exist := s.clients[id]; exist {
-				delete(s.clients, id)
-			} else if job, exist := s.miners[id]; exist {
-				if job != nil {
-					s.jobQueue.PushFront(*job)
-				}
-				delete(s.miners, id)
-			}
-		}
-		s.dispatchJobsToMiners()
-	}
-}
-
-func (s *server) receiveMessage() {
-	for {
-		id, bytes, err := s.lspServer.Read()
-		if err != nil {
-			s.connLost <- id
-		} else {
-			msg := &bitcoin.Message{}
-			if err := json.Unmarshal(bytes, msg); err != nil {
-				continue
-			}
-			s.receiveMsg <- idMsgBundle{id, msg}
-		}
-	}
-}
-
-func (s *server) sendMessage(id int, msg *bitcoin.Message) error {
-	bytes, err := json.Marshal(msg)
-	if err != nil {
-		return err
-	}
-	if err := s.lspServer.Write(id, bytes); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (s *server) addJob(id int, task *bitcoin.Message) {
-	newRequest := &request{0, ^uint64(0), uint64(0)}
-	lower, upper := task.Lower, task.Lower+maxJobSize-1
-
-	for upper < task.Upper {
-		newRequest.jobsRemain++
-		s.jobQueue.PushBack(job{id, task.Data, lower, upper})
-		lower = upper + 1
-		upper = lower + maxJobSize - 1
-	}
-	newRequest.jobsRemain++
-	s.jobQueue.PushBack(job{id, task.Data, lower, task.Upper})
-	s.clients[id] = newRequest
-}
-
-func (s *server) findIdleMiner() (int, bool) {
-	for minerId, job := range s.miners {
-		if job == nil {
-			return minerId, true
-		}
-	}
-	return 0, false
-}
-
-func (s *server) dispatchJobsToMiners() {
-	for s.jobQueue.Len() > 0 {
-		job := s.jobQueue.Front().Value.(job)
-		if _, exist := s.clients[job.clientid]; exist {
-			if minerId, exist := s.findIdleMiner(); exist {
-				s.miners[minerId] = &job
-				s.sendMessage(minerId, bitcoin.NewRequest(job.message, job.minNonce, job.maxNonce))
-			} else {
-				return
-			}
-		}
-		s.jobQueue.Remove(s.jobQueue.Front())
-	}
+	// TODO: implement this!
+	go srv.readRoutine()
+	srv.mainRoutine()
+	
 }
